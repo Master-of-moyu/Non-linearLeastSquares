9921aef02802acf8c061cdf944cf08c6696c1204
xiangqian <648261121@qq.com>
2020-01-02 22:58:32 +0800

    Clean LM.

--- src/curve_fitting_LM.cpp
+++ src/curve_fitting_LM.cpp
@@ -36,48 +36,111 @@ class LMOptimization {
         c_ = c;
     }
 
+    void compute_residual_jacobian(MatrixXd& Jacobian, VectorXd& errors, double m, double c) {
+        Jacobian.setZero();
+        errors.setZero();
+        for (int k = 0; k < errorTerms.size(); k++) {
+            errors[k] = errorTerms[k]->function(m, c);
+            Jacobian.row(k) = errorTerms[k]->Jacobian(m, c);
+        }
+    }
+
     void OptimizeLM(int niters) {
+        double cost_old = 0.0, cost_new = 0.0;
         double lambda = 0.01;
+        MatrixXd Jacobian(errorTerms.size(), 2);
+        VectorXd errors(errorTerms.size());
+
+        compute_residual_jacobian(Jacobian, errors, m_, c_);
+        cost_old = 0.5 * errors.squaredNorm();
+
         for (int iter = 0; iter < niters; iter++) {
-            MatrixXd Jacobian(errorTerms.size(), 2);
-            VectorXd err(errorTerms.size());
-
-            for (int k = 0; k < errorTerms.size(); k++) {
-                err[k] = errorTerms[k]->function(m_, c_);
-                Jacobian.row(k) = errorTerms[k]->Jacobian(m_, c_);
-            }
-
-            Matrix2d JTJ = Jacobian.transpose() * Jacobian;                    // hessian
-            Matrix2d H = JTJ + lambda * Matrix2d(JTJ.diagonal().asDiagonal()); // LM
-            Vector2d b = -Jacobian.transpose() * err;
+            Matrix2d JTJ = Jacobian.transpose() * Jacobian; // hessian
+            Matrix2d H = JTJ;
+            Vector2d b = -Jacobian.transpose() * errors;
+            for (int i = 0; i < H.cols(); i++)
+                H(i, i) *= (1 + lambda);
             //solve
             Vector2d delta = H.colPivHouseholderQr().solve(b);
 
-            if (delta.norm() < 1e-8) {
+            if ((bool)std::isnan((double)delta[0])) {
+                // T_new = T_old;
+                break;
+            }
+
+            if (delta.maxCoeff() < 1e-8) {
                 log_debug("converged!");
                 break;
             }
 
-            double err_before = 0.5 * err.squaredNorm();
-            double err_after = 0;
-            for (int k = 0; k < errorTerms.size(); k++) {
-                double e = errorTerms[k]->function(m_ + delta[0], c_ + delta[1]);
-                err_after += e * e;
-            }
-            err_after *= 0.5;
-            if (err_after < err_before) {
+            // update
+            cost_new = 0.0;
+            m_ += delta[0];
+            c_ += delta[1];
+            compute_residual_jacobian(Jacobian, errors, m_, c_);
+            cost_new = 0.5 * errors.squaredNorm();
+            if (cost_new < cost_old) {
                 // accept and update
-                m_ += delta[0];
-                c_ += delta[1];
                 lambda /= 2;
-                err_before = err_after;
+                cost_old = cost_new;
+                if (lambda < 1e-6) lambda = 1e-6;
             } else {
-                lambda *= 4;
+                m_ -= delta[0];
+                c_ -= delta[1];
+                compute_residual_jacobian(Jacobian, errors, m_, c_);
+                lambda *= 3;
+                if (lambda > 1000) lambda = 1000;
             }
 
-            log_debug("iter: %d, cost: %lf, lambda: %lf", iter, err_before, lambda);
+            log_debug("iter: %d, cost: %lf, lambda: %lf", iter, cost_old, lambda);
         }
     }
+
+    void OptimizeDogLeg(int niters) {
+        double cost_old = 0.0, cost_new = 0.0;
+        MatrixXd Jacobian(errorTerms.size(), 2);
+        VectorXd errors(errorTerms.size());
+
+        compute_residual_jacobian(Jacobian, errors, m_, c_);
+        cost_old = 0.5 * errors.squaredNorm();
+
+        for (int iter = 0; iter < niters; iter++) {
+            Matrix2d JTJ = Jacobian.transpose() * Jacobian; // hessian
+            Matrix2d H = JTJ;
+            Vector2d b = -Jacobian.transpose() * errors;
+            //solve
+            Vector2d delta = H.colPivHouseholderQr().solve(b);
+
+            if ((bool)std::isnan((double)delta[0])) {
+                // T_new = T_old;
+                break;
+            }
+
+            if (delta.maxCoeff() < 1e-8) {
+                log_debug("converged!");
+                break;
+            }
+
+            // update
+            cost_new = 0.0;
+            m_ += delta[0];
+            c_ += delta[1];
+            compute_residual_jacobian(Jacobian, errors, m_, c_);
+            cost_new = 0.5 * errors.squaredNorm();
+            if (cost_new < cost_old) {
+                // accept and update
+                cost_old = cost_new;
+            } else {
+                m_ -= delta[0];
+                c_ -= delta[1];
+                log_debug("cost increased. iter: %d", iter);
+                break;
+            }
+
+            log_debug("iter: %d, cost: %lf", iter, cost_old);
+        }
+    }
+
     void addErrorTerm(CostBlock* e) {
         errorTerms.push_back(e);
     }
@@ -108,6 +171,12 @@ int main() {
     opt.setInitValue(0, 0);
     opt.OptimizeLM(50);
 
+    log_debug("estimated a: %lf, b: %lf", opt.m_, opt.c_);
+    log_debug("");
+    log_debug("opt again:");
+
+    opt.setInitValue(0, 0);
+    opt.OptimizeDogLeg(50);
     log_debug("estimated a: %lf, b: %lf", opt.m_, opt.c_);
     return 0;
 }
